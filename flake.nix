e9909f5d837645f06d250a2deb33cde84a0f81f5
Wes McKinney <wesmckinn+git@gmail.com>
2026-03-03 16:01:05 -0600

    Update flake.nix for v0.40.0 (#420)
    Updates flake.nix version to 0.40.0 for the v0.40.0 release.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         packages = {
           default = pkgs.buildGoModule {
             pname = "roborev";
-            version = "0.39.0";
+            version = "0.40.0";
 
             src = ./.;
 
